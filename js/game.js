9934206799e0798a5779debdb9c1a2d312e5a324
Juliene Flora <flora.juliene@gmail.com>
2020-09-16 16:03:34 -0300

    Projeto Finalizado

--- js/game.js
+++ js/game.js
@@ -44,10 +44,67 @@ espacos[i].addEventListener ("click", function(){
             playTime = player1;
         }
         atualizaMostrador();
+        verificarVencedor();
 
     }
 });
 
 }
+
+}
+
+ async function verificarVencedor() {
+
+    var a1 = document.getElementById("a1").getAttribute("jogada");
+    var a2 = document.getElementById("a2").getAttribute("jogada");
+    var a3 = document.getElementById("a3").getAttribute("jogada");
+    
+    
+    var b1 = document.getElementById("b1").getAttribute("jogada");
+    var b2 = document.getElementById("b2").getAttribute("jogada");
+    var b3 = document.getElementById("b3").getAttribute("jogada");
+    
+    
+    var c1 = document.getElementById("c1").getAttribute("jogada");
+    var c2 = document.getElementById("c2").getAttribute("jogada");
+    var c3 = document.getElementById("c3").getAttribute("jogada");
+    
+    var vencedor = "";
+    
+    if((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "" ) || (a1 == b2 && a1 == c3 && a1 != "")){
+        
+    vencedor = a1;
+    
+    }else if((b2 == b1 && b2 == b3 && b2 != "") || (b2 == a2 && b2 == c2 && b2 != "" ) || (b2 == a3 && b2 == c1 && b2 != "")
+    )
+    
+    {
+        vencedor = b2;
+    
+    }else if(((c3 == c2 && c3 == c1 )||(c3 == a3 && c3 == b3)) && c3 != "")
+    {
+       vencedor = c3;
+
+    }
+
+    
+    
+    
+
+    if (vencedor != "") {
+        gameOver = true;
+
+        await sleep(50);
+
+        alert("O ganhador foi o '" + vencedor + "'");
+    } if((a1 != "" && a2 != "" && a3 != "" && b1 !="" && b2 !="" && b3 != "" && c1 != "" && c2 !="" && c3 !="" && vencedor == "")) {
+        alert("Deu velha !!!")
+    }
+    
+}
+
+function sleep(ms) {
+
+    return new Promise(resolve => setTimeout(resolve, ms));
 
 }
